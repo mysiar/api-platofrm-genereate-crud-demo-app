b3ad49b0cab77b0d3e0ea25c1a9068ca9149b624
Piotr Synowiec <psynowiec@gmail.com>
2017-08-14 12:13:58 +0200

    update

--- skeleton/src/HomePage.js
+++ skeleton/src/HomePage.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class HomePage extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="">
         <h1>API Platform - generate crud - demo app</h1>
       </div>
     );
--- skeleton/src/NotFound.js
+++ skeleton/src/NotFound.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class NotFound extends Component {
   render () {
     return (
-      <div className="container">
+      <div className="">
         <h2>Page not found</h2>
       </div>
     )
